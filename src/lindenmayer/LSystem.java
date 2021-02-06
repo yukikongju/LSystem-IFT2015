c2bae32cbaa29de0d8072892a2d6165ff9b67020
Emulie Chhor <emulie.chhor@hotmail.com>
2021-02-06 10:57:49 -0500

    implemented LSystem class with empty functions from its AbstractLSystem abstraction

--- src/lindenmayer/LSystem.java
+++ src/lindenmayer/LSystem.java
@@ -4,18 +4,77 @@
  * and open the template in the editor.
  */
 package lindenmayer;
+import java.awt.geom.Rectangle2D;
+import java.util.HashMap;
+import java.util.HashSet;
 import org.json.*;
 
 /**
  *
  * @author emuli
  */
-public class LSystem {
+public class LSystem extends AbstractLSystem {
+    
+    private String file;
+    private HashSet alphabet, axiom;
+    private HashMap rules, actions;
+    private int steps;
+    private double angle;
+    private int[] position;
+    //private Turtle turtle;
+    private TurtleUI turtle;
+    
+    /** TODO: Constructor **/
+    public LSystem(String file){
+        this.file = file;
+    }
     
     public static void readJSONFile(String file, LSystem S, Turtle T) throws java.io.IOException {
-        JSONObject input = new JSONObject(new JSONTokener(new java.io.FileReader(file))); // lecture de fichier JSON avec JSONTokener
-        JSONArray alphabet = input.getJSONArray("alphabet");
-        String axiom = input.getString("axiom");
-        //to continue
+        
+    }
+
+    @Override
+    public Symbol addSymbol(char sym) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addRule(Symbol sym, String expansion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAction(Symbol sym, String action) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAxiom(String str) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Symbol.Seq getAxiom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Symbol.Seq rewrite(Symbol sym) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void tell(Turtle turtle, Symbol sym) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Symbol.Seq applyRules(Symbol.Seq seq, int n) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Rectangle2D tell(Turtle turtle, Symbol sym, int rounds) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
