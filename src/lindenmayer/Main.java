debdb2f52d370aa6f7f1b6f188998b00fe1ca3d5
Emulie Chhor <emulie.chhor@hotmail.com>
2021-02-21 20:07:21 -0500

    commit before MainFrame

--- src/lindenmayer/Main.java
+++ src/lindenmayer/Main.java
@@ -16,6 +16,7 @@ public class Main {
         String file = "test/buisson.json";
         String file2 = "test/hexamaze.json";
         String file3 = "test/herbe.json";
+        String file4 = "test/sierpinski.json";
 
         // 2. Read num iterations
 //        int rounds = Integer.parseInt(args[0]);
@@ -25,8 +26,8 @@ public class Main {
         // 3. Initialize LSystem
 //        LSystem system = new LSystem(file2, numIter);
 //        LSystem system = new LSystem(file3, rounds);
-        MainPS mainPS = new MainPS(file, rounds);
-//        mainPS.printPostScript();
+        MainPS mainPS = new MainPS(file4, rounds);
+        mainPS.printPostScript();
         
 //        https://stackoverflow.com/questions/1676187/why-is-paint-paintcomponent-never-called
         // We need Swing Utilities to make sure paint(0 doesn't run before the constructor
@@ -35,7 +36,7 @@ public class Main {
             public void run() {
                 GUI gui;
                 try {
-                    gui = new GUI(file2, rounds);
+                    gui = new GUI(file, rounds);
                     gui.setVisible(true);
                 } catch (IOException ex) {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
